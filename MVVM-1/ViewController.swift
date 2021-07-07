d165dd6724344277b5728978fdf1995478635168
EvgeniyKostin <kostinen@icloud.com>
2021-07-07 22:38:02 +0300

    Initial Commit

---
+++ MVVM-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-1
+//
+//  Created by Evgeniy Kostin on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
